28f9375a6f5ae31a1114e3fed68387f517fc55d9
Nathanael Vieira <59123681+NathanaelV@users.noreply.github.com>
2023-08-28 13:36:35 -0300

    F1API-869: Create consortium filter

--- spec/hooks_spec.rb
+++ spec/hooks_spec.rb
@@ -1,67 +1,79 @@
 require 'ostruct'
 
 RSpec.describe F1SalesCustom::Hooks::Lead do
-  context 'when lead come from myHonda' do
-    let(:lead) do
-      lead = OpenStruct.new
-      lead.source = source
-      lead.product = product
-      lead.description = ''
+  describe '.switch_source' do
+    context 'when lead come from myHonda' do
+      let(:lead) do
+        lead = OpenStruct.new
+        lead.source = source
+        lead.product = product
+        lead.description = ''
 
-      lead
-    end
-
-    let(:source) do
-      source = OpenStruct.new
-      source.name = 'myHonda'
-
-      source
-    end
-
-    let(:product) do
-      product = OpenStruct.new
-      product.name = ''
-
-      product
-    end
-
-    let(:switch_source) { described_class.switch_source(lead) }
-
-    context 'when description do not contains dealership code' do
-      it 'returns original source' do
-        expect(switch_source).to eq('myHonda')
+        lead
       end
-    end
 
-    context 'when description contains code 1709446' do
-      before { lead.description = 'Concessionária: HIT ILHA - Código: 1709446' }
+      let(:source) do
+        source = OpenStruct.new
+        source.name = 'myHonda'
 
-      it 'returns Ilha in source' do
-        expect(switch_source).to eq('myHonda')
+        source
       end
-    end
 
-    context 'when description contains code 1699751' do
-      before { lead.description = 'Concessionária: HIT ILHA - Código: 1699751' }
+      let(:product) do
+        product = OpenStruct.new
+        product.name = ''
 
-      it 'returns Ilha in source' do
-        expect(switch_source).to eq('myHonda')
+        product
       end
-    end
 
-    context 'when description contains code 1699751 and Tipo Consórcio' do
-      before { lead.description = 'Concessionária: HIT ILHA - Código: 1699751 - Tipo: Consórcio' }
+      let(:switch_source) { described_class.switch_source(lead) }
 
-      it 'returns Ilha in source' do
-        expect(switch_source).to eq('myHonda - Consórcio')
+      context 'when description do not contains dealership code' do
+        it 'returns original source' do
+          expect(switch_source).to eq('myHonda')
+        end
       end
-    end
 
-    context 'when product is revisão' do
-      before { product.name = 'WRV - Revisão' }
+      context 'when description contains code 1709446' do
+        before { lead.description = 'Concessionária: HIT ILHA - Código: 1709446' }
 
-      it 'returns Fonte sem time' do
-        expect(switch_source).to eq('myHonda - Revisão')
+        it 'returns Ilha in source' do
+          expect(switch_source).to eq('myHonda')
+        end
+      end
+
+      context 'when description contains code 1699751' do
+        before { lead.description = 'Concessionária: HIT ILHA - Código: 1699751' }
+
+        it 'returns Ilha in source' do
+          expect(switch_source).to eq('myHonda')
+        end
+      end
+
+      context 'when is to Consórcio Team' do
+        context 'when description contains code 1699751 and Tipo Consórcio' do
+          before { lead.description = 'Concessionária: HIT ILHA - Código: 1699751 - Tipo: Consórcio' }
+
+          it 'returns Ilha in source' do
+            expect(switch_source).to eq('myHonda - Consórcio')
+          end
+        end
+
+        context 'when description contains HSF' do
+          before { lead.description = 'Concessionária: HIT ILHA; Código: 1709446; Tipo: HSF' }
+
+          it 'returns Ilha in source' do
+            expect(switch_source).to eq('myHonda - Consórcio')
+          end
+        end
+      end
+
+      context 'when product is revisão' do
+        before { product.name = 'WRV - Revisão' }
+
+        it 'returns Fonte sem time' do
+          expect(switch_source).to eq('myHonda - Revisão')
+        end
       end
     end
   end
